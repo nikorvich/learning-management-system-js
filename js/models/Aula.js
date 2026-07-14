0dbecef605fa8a2e280d1ba0939a765a188d4de7
nikorvich <134230527+nikorvich@users.noreply.github.com>
2026-07-14 10:57:55 -0300

    Create Aula class for course module management

---
+++ js/models/Aula.js
@@ -0,0 +1,9 @@
+export class Aula {
+    constructor(id, modId, titulo, ordem) {
+        this.ID_Aula = id;
+        this.ID_Modulo = modId;
+        this.Titulo = titulo;
+        this.Ordem = ordem;
+        this.TipoConteudo = 'Vídeo';
+    }
+}
